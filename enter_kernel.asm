1e208ea22bb05bfd1d7b7bde327d34d56062e29f
xiangui <yaowentao1216@126.com>
2025-03-08 01:49:55 +0800

    wqe

--- enter_kernel.asm
+++ enter_kernel.asm
@@ -6,5 +6,15 @@
 ; Code for jumpting to C kernel.
 ; Implement your routine for entering the kernel here
 ;
-
-; ...
+%include "print_string.asm"
+extern kernel_main
+section .text
+global enter_kernel
+SUECCSS_ENTER: db "Successfully entered mode", 0
+enter_kernel:
+    mov ebx, SUECCSS_ENTER
+    call print_string_pm
+    mov esp, 0x9000
+    call kernel_main
+    
+    jmp $
